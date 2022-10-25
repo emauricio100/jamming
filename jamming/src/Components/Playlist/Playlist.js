5131070bbb9611a3ea7f5a27bb272c6c4209c507
emauricio100 <emauricio@ucdavis.edu>
2022-10-25 13:16:55 -0700

    update

--- jamming/src/Components/Playlist/Playlist.js
+++ jamming/src/Components/Playlist/Playlist.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
+import './Playlist.css';
 import TrackList from '../TrackList/TrackList';
-import './Playlist.css'
-import '../TrackList/TrackList';
 class Playlist extends React.Component {
-    redner() {
-        return(
+    render() {
+        return (
         <div className="Playlist">
-            <input defaultValue={"New Playlist"}/>
+            <input defaultValue="New Playlist"/>
             {/* <TrackList /> */}
             <button className="Playlist-save">SAVE TO SPOTIFY</button>
         </div>
         )
-    }
+}
 }
 export default Playlist;
